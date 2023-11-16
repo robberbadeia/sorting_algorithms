1cb9dad4920c126d606472dd0489c562c7c3ee2f
robberbadeia <robber.badeia@gmail.com>
2023-11-16 14:09:17 +0200

    T00

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - Function
+ * @array: pointer to array
+ * @size: input array size
+ * Return: None
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j = 0;
+	int tmp = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				print_array(array, size);
+			}
+		}
+	}
+}
