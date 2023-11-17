2cf5cf1d7e8063c8c1e5f3a59387eca6cf60a7a8
robberbadeia <robber.badeia@gmail.com>
2023-11-17 15:13:44 +0200

    T00

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp = 0;
 
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
 
 	for (i = 0; i < size - 1; i++)
@@ -22,7 +22,6 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-
 				print_array(array, size);
 			}
 		}
