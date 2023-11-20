5c3c21b63b2920154a4ac7a8bd88dcbff364397a
robberbadeia <robber.badeia@gmail.com>
2023-11-20 18:55:47 +0200

    .

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,9 +23,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		tmp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = tmp;
-		print_array(array, size);
+		if (array[min_idx] != array[i])
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
 	}
 }
