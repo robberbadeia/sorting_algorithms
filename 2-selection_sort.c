c7943bc85f1e3b62db4957fbd22df6cccff1be07
robberbadeia <robber.badeia@gmail.com>
2023-11-20 18:40:37 +0200

    .

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+void swap (int *min, int *b)
+{
+	printf("min:%ls, bigger:%ls", min, b);
+}
+/**
+ * selection_sort - Function
+ * @array: array of int
+ * @size: int
+ * Return: None
+*/
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min_idx;
+	int tmp;
+
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		// swap function between index i and j
+		tmp = array[i];
+		array[i] = array[min_idx];
+		array[min_idx] = tmp;
+		print_array(array, size);
+	}
+}
