2a4810eb9d9382987cd642807ae8b6f59304083c
robberbadeia <robber.badeia@gmail.com>
2023-11-20 21:25:08 +0200

    .

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+/**
+ * swap_elements - Function
+ * @f: input
+ * @s: input
+ * Return: None
+*/
+void swap_elements(int *f, int *s)
+{
+	int tmp = *f;
+	*f = *s;
+	*s = tmp;
+}
+
+/**
+ * partition_function - Function
+ * @array: input
+ * @l: input
+ * @h: input
+ * Return: int
+*/
+int partition_function(int *array, int l, int h)
+{
+	int p = array[h];
+	int i = l - 1;
+	int j;
+
+	for (j = l; j <= h - 1; j++)
+	{
+		if (array[j] < p)
+		{
+			i++;
+			swap_elements(&array[i], &array[j]);
+		}
+	}
+	swap_elements(&array[i + 1], &array[h]);
+	return (i + 1);
+}
+
+/**
+ * sort_quick - Function
+ * @array: input
+ * @l: input
+ * @h:input
+ * Return: None
+*/
+void sort_quick(int *array, int l, int h)
+{
+	int p_index = 0;
+
+	if (l < h)
+	{
+		p_index = partition_function(array, l, h);
+		sort_quick(array, l, (p_index - 1));
+		sort_quick(array, (p_index + 1), h);
+	}
+}
+
+/**
+ * quick_sort - Function
+ * @array: input
+ * @size: array size
+ * Return: None
+*/
+void quick_sort(int *array, size_t size)
+{
+	int l = 0;
+	int h = size - 1;
+
+	sort_quick(array, l, h);
+}
